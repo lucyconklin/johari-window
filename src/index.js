b87c45249837bbbf4377e06959e21538704a085d
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-03-29 16:22:23 -0600

    Render app component on id root

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import './index.css';
 
-ReactDOM.render(<App />,document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
