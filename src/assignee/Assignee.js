4dd027d8af1136f85a19608108b609471e146bc1
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-03-30 19:16:21 -0600

    Render inactive links for completed assignments
    Use boolean completed prop to render an incomplete assignment or
    completed assignment accordingly.

--- src/assignee/Assignee.js
+++ src/assignee/Assignee.js
@@ -3,13 +3,31 @@ import { Link } from 'react-router-dom';
 import './Assignee.css';
 
 class Assignee extends Component {
+  constructor() {
+    super();
+    this.completedAssignee = this.completedAssignee.bind(this)
+    this.incompleteAssignee = this.incompleteAssignee.bind(this)
+  }
 
-  render() {
+  completedAssignee() {
     return (
-      <div className="Assignee">
+      <div className={'Assignee completed-assignee'}>
+        <a>{ this.props.name }</a>
+      </div>
+    )
+  }
+
+  incompleteAssignee() {
+    return (
+      <div className={'Assignee incomplete-assignee'}>
         <Link to={'/johari/' + this.props.id}>{ this.props.name }</Link>
       </div>
-    );
+    )
+  }
+
+  render() {
+    if (this.props.completed) { return this.completedAssignee() }
+    else { return this.incompleteAssignee() }
   }
 }
 
