a3d8222ad0f9f73d2da09a976aef4c52337ed0ab
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-03 17:16:43 -0600

    Adds App.css to App, some test styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./App.css"
 
 import Main from '../main/Main';
 import Johari from '../johari/Johari';
