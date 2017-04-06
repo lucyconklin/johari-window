331522c516c01f40375fadf4c43714a0d0a3d324
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-06 13:54:27 -0600

    Finishes API call for description

--- src/my-window/MyWindow.js
+++ src/my-window/MyWindow.js
@@ -4,7 +4,12 @@ import './MyWindow.css';
 class MyWindow extends Component {
   constructor() {
     super();
-    this.state = { myWindow: [] };
+    this.state = { myWindow: {
+      arena: [],
+      facade: [],
+      blindSpot: [],
+      unknown: []
+    } };
 
     this.getMyWindow = this.getMyWindow.bind(this);
   }
@@ -14,11 +19,14 @@ class MyWindow extends Component {
   }
 
   getMyWindow () {
-    fetch('/')
-      .then(result => result.json())
+    fetch('https://johariwindowapi.herokuapp.com/api/v1/users/1/descriptions')
+      .then(result => result.json() )
       .then(data => {
-        this.setState({ myWindow: data })
-        localStorage.setItem('myWindow', data)
+        this.state.myWindow['arena'] = data['arena']
+        this.state.myWindow['facade'] = data['facade']
+        this.state.myWindow['blindSpot'] = data['blind-spot']
+        this.state.myWindow['unknown'] = data['unknown']
+        this.setState( this.state )
         return true
       })
   }
@@ -29,15 +37,16 @@ class MyWindow extends Component {
         <h2>My Window</h2>
         <div className='johari-window'>
           <table>
-          <th></th>
-          <tr>
-            <td className="johari-windowpane-title">Arena</td>
-            <td className="johari-windowpane-title">Blind Spot</td>
-          </tr>
-          <tr>
-            <td className="johari-windowpane-title">Fa&#231;ade</td>
-            <td className="johari-windowpane-title">Unknown</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="johari-windowpane-title">Arena</td>
+              <td className="johari-windowpane-title">Blind Spot</td>
+            </tr>
+            <tr>
+              <td className="johari-windowpane-title">Fa&#231;ade</td>
+              <td className="johari-windowpane-title">Unknown</td>
+            </tr>
+          </tbody>
           </table>
         </div>
       </div>
