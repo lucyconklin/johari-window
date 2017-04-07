4f46dca8478640bdaa657bf9ebc2fc7b49062262
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-07 14:52:57 -0600

    Changes opacity with frequency for adjectives

--- src/my-window-adjective/MyWindowAdjective.js
+++ src/my-window-adjective/MyWindowAdjective.js
@@ -4,7 +4,8 @@ import './MyWindowAdjective.css'
 class MyWindowAdjective extends Component {
 
   render() {
-    const style = { opacity: this.props.frequency }
+    const opacity = (this.props.frequency/2) + .5
+    const style = { opacity: opacity }
     return (
       <div className='MyWindowAdjective'>
         <li className='johari-window-adjective' style={ style }>{ this.props.name }</li>
