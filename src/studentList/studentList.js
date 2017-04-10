82c1539eb7d34ae31d41812115b531ded0845a5a
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-10 17:16:35 -0600

    Add studentList component
    studentList Component is rendered in the Cohort component, and
    stuentComponent renders the individual students. Small bug here to
    figure out tomorrow.

---
+++ src/studentList/studentList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './studentList.css';
+import Student from '../student/Student';
+
+
+class studentList extends Component {
+  constructor(){
+    super();
+    this.state = {students: []};
+  }
+
+  componentDidMount(){
+    this.retrieveStudents();
+  }
+
+  retrieveStudents(){
+    var that = this;
+    fetch('https://johariwindowapi.herokuapp.com/api/v1/cohorts/' + that.props.cohortID + '/users')
+      .then(result => result.json())
+      .then(data => {
+        that.setState({ students: data })
+        return true
+    })
+  }
+
+  eachStudent(student, i){
+    return <Student key={i} name={student.name} id={student.id} />
+  }
+  render() {
+    return (
+      <div className='StudentList'>
+        <h1>Select students to create groups.</h1>
+        <div className='student-list'>
+          { this.state.students.map(this.eachStudent) }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default studentList;
