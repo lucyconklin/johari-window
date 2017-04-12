4ca5d1553fa854e2077ba0317b8e8a52c0b91cdc
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-12 16:22:59 -0600

    Add user name to page from local storage

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import './Main.css';
+import User from '../user/User'
 import Header from '../header/Header';
 import AssigneeList from '../assignee-list/AssigneeList';
 
 class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+    user: new User(JSON.parse(localStorage.getItem('user')))
+  }
+  }
   render() {
     return (
       <div className='Main'>
